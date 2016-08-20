637e65ac42fc8292603458155cd4c9db5a25b5fa
kushariadi <kushariadikoing@gmail.com>
2016-08-20 14:25:13 +0700

    level2

---
+++ src/marathongit2/Terbilang.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marathongit2;
+
+/**
+ *
+ * @author Jhon
+ */
+public class Terbilang {
+    public static String proses(int value) {
+        String[] bilangan = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
+        String temp = " ";
+        if (value < 12) {
+            temp = " " + bilangan[value];
+        } else if (value < 20) {
+            temp = proses(value - 10) + " belas";
+        } else if (value < 100) {
+            temp = proses(value / 10) + " puluh" + proses(value % 10);
+        } else if (value < 200) {
+            temp = " seratus" + proses(value - 100);
+        } else if (value < 1000) {
+            temp = proses(value / 100) + " ratus" + proses(value % 100);
+        } else if (value < 2000) {
+            temp = "seribu" + proses(value - 1000);
+        } else if (value < 1000000) {
+            temp = proses(value / 1000) + " ribu" + proses(value % 1000);
+        }
+        return temp;
+}
+}
