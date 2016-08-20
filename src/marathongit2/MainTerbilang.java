4979e93f31718800c16bc1ac6373973ca3dfead5
kushariadi <kushariadikoing@gmail.com>
2016-08-20 14:38:05 +0700

    level2(sempurna)

--- src/marathongit2/MainTerbilang.java
+++ src/marathongit2/MainTerbilang.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package marathongit2;
 
-/**
- *
- * @author Jhon
- */
+package marathongit2;
+import marathongit2.Terbilang;
+
 public class MainTerbilang {
-    Terbilang terbilang = new Terbilang();
+   
+
+
+public static void main (String[] args){
+System.out.println(Terbilang.proses(27400));
+}
 }
